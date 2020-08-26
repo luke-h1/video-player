f52ddff4d5c1cd490fbd4ca7514b5d92b34cb927
luke-h1 <luke.howsam@yahoo.com>
2020-08-26 14:35:03 +0100

    add event listeners

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-const video = document.getElementById('video')
+const video = document.getElementById('video');  
+const play = document.getElementById('play'); 
+const stop = document.getElementById('stop'); 
+const progress = document.getElementById('progress'); 
+const timestamp = document.getElementById('timestamp'); 
+
+
+
+
+
+
+
+
+
+// EVENT LISTENERS 
+video.addEventListener('click', toggleVideoStatus); 
+video.addEventListener('pause', updatePlayIcon); 
+video.addEventListener('play', updatePlayIcon); 
+video.addEventListener('timeupdate', updateProgress); 
+
+play.addEventListener('click', toggleVideoStatus); 
+
+stop.addEventListener('click', stopvideo); 
+
+progress.addEventListener('change', setVideoProgress); 
